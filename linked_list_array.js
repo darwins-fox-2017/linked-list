0ad411f159d4ecd98df4247ad4e128c76ff104a2
adamsaparudin <adamsaparudin@gmail.com>
2017-02-02 08:53:48 +0700

    linked list selesai mas/mba

--- linked_list_array.js
+++ linked_list_array.js
@@ -1,4 +1,18 @@
 //write your code here
+class LinkedList {
+  constructor(lister) {
+    this.lister = lister
+  }
+  getLength() {
+    return this.lister.length
+  }
+  getHead() {
+    return this.lister[0]
+  }
+  getTail() {
+    return this.lister[this.lister.length-1]
+  }
+}
 
 let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(list.getLength());
--- linked_list_queue.js
+++ linked_list_queue.js
@@ -1,4 +1,26 @@
+'use strict'
 //write your code here
+
+class LinkedList {
+  constructor(lister) {
+    this.lister = lister
+  }
+  getLength() {
+    return this.lister.length
+  }
+  getHead() {
+    return this.lister[0]
+  }
+  getTail() {
+    return this.lister[this.lister.length-1]
+  }
+  push(x) {
+    this.lister.push(x)
+  }
+  pop() {
+    this.lister.pop()
+  }
+}
 
 let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(list.getLength());
